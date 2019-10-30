3bacd870f1a08cd705b1e82a74e46663b208455b
benny83 <benny.fox.83@gmail.com>
2019-10-30 21:26:21 +0300

    fix pagination

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,5 @@
 $(function() {
+  $('.pagination a').attr('data-remote', 'true')
   $('#company, #gender, #active').on('change', function() {
     $.get($("#users_search").attr("action"), $("#users_search").serialize(), null, "script");
     return false;
